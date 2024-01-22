9081de10c9c1161aefe98fbe93bcb156e64900c6
Mario diaz <ironmario25@hotmail.com>
2024-01-22 15:59:13 -0300

    v1.1

--- src/navBar/ResponsivAppBar.jsx
+++ src/navBar/ResponsivAppBar.jsx
@@ -36,7 +36,7 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{background:'black',marginBottom:2}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
